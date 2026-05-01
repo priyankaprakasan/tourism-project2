7122f4cde9210277315f975b9309ccced6a787d8
Priyanka Pazhayedath Prakasan <priyankaprakasan@gmail.com>
2026-05-01 16:15:06 +0200

    Update repo_id for uploading to new space

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="deployment",     # the local folder containing your files
-    repo_id="priyankaprakasan/tourism_project",          # the target repo
+    repo_id="priyankaprakasan/tourism-space",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
